1956a9924cc4af4ee40579ae99079e258e1fab3d
CeluiQuiSait <eric.lavergne@telecom-sudparis.eu>
2018-04-25 00:56:20 +0200

    Ajout du mini-jeu l'arche de Noe dans un dossier separe

---
+++ ArcheDeNoe/app/src/main/java/com/example/eric/archedenoe/ArcheDeNoeActivity.java
@@ -0,0 +1,112 @@
+package com.example.eric.archedenoe;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.Math;
+
+import static android.view.View.GONE;
+
+public class ArcheDeNoeActivity extends Activity {
+
+    private ImageView elephant;
+    private ImageView arche;
+    private ImageView lion;
+    private ImageView lapin;
+    private ImageView mer;
+    private FrameLayout layout;
+    List<ImageView> animauxArche;
+    public ImageView animal;
+    private int nbAnimaux = 0;
+    private int posArche;
+    private int coule;
+    private DisplayMetrics metrics;
+    private float hauteurEcran;
+    private float densiteEcran;
+    private float hauteurArche;
+
+    private OnClickListener clickListenerAnimaux = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            animal=new ImageView(ArcheDeNoeActivity.this);
+            switch (v.getId()){
+                case R.id.elephant:
+                    coule=24+(int)(5*Math.random());
+                    layout.addView(animal,150,150);
+                    animal.setImageResource(R.drawable.elephantdebout);
+                    animal.setY(posArche+210);
+                    break;
+                case R.id.lion:
+                    coule=15+(int)(3*Math.random());
+                    layout.addView(animal,120,120);
+                    animal.setImageResource(R.drawable.lion);
+                    animal.setY(posArche+230);
+                    //animauxArche[nbAnimaux].setImageResource(R.drawable.lion);
+                    break;
+                case R.id.lapin:
+                    coule=3+(int)(2*Math.random());
+                    layout.addView(animal,65,65);
+                    animal.setImageResource(R.drawable.lapin);
+                    animal.setY(posArche+250);
+                    //animauxArche[nbAnimaux].setImageResource(R.drawable.lapin);
+                    break;
+            }
+            animal.setX((float)(Math.random()*240+170)*densiteEcran);
+            arche.bringToFront();
+            mer.bringToFront();
+            nbAnimaux+=1;
+            posArche+=coule;
+            arche.setY(posArche);
+            for(ImageView anim : animauxArche){
+                anim.setY(anim.getY()+coule);
+            }
+            animauxArche.add(animal);
+            //L'arche coule lorsque une certaine partie de l'image est submergée (49pxls au-dessus de la moitié)
+            //Un schéma aide... hauteurArche=(478/1080)*(350*dens)
+            //Bon pour l'instant j'ai bidouillé avec un terme en plus pour que ça marche...
+            if ((hauteurEcran-posArche-154.9/2*densiteEcran+49*((float)350/(float)1080)*densiteEcran-114.5*densiteEcran)<80*densiteEcran){
+                arche.setVisibility(GONE);
+                elephant.setVisibility(GONE);
+                lion.setVisibility(GONE);
+                lapin.setVisibility(GONE);
+                for (ImageView anim : animauxArche) {
+                    anim.setVisibility(GONE);
+                }
+                //Message de fin et retour...
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arche_de_noe);
+        elephant=(ImageView) findViewById(R.id.elephant);
+        lion=(ImageView) findViewById(R.id.lion);
+        lapin=(ImageView) findViewById(R.id.lapin);
+        arche=(ImageView) findViewById(R.id.arche);
+        mer=(ImageView)findViewById(R.id.mer);
+        layout=(FrameLayout) findViewById(R.id.layout);
+        hauteurArche=arche.getMeasuredHeight();
+        elephant.setOnClickListener(clickListenerAnimaux);
+        lion.setOnClickListener(clickListenerAnimaux);
+        lapin.setOnClickListener(clickListenerAnimaux);
+
+        animauxArche = new ArrayList<ImageView>();
+
+        metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        hauteurEcran=metrics.heightPixels;
+        densiteEcran=metrics.density;
+        posArche=(int)(hauteurEcran-(175+130)*densiteEcran);//175<->moitié de la hauteur de l'image;130<->surface de l'eau+marge
+        arche.setY(posArche);
+    }
+}
