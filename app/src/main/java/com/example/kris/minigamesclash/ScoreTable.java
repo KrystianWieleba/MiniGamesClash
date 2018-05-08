653ad2e473cf6a03618d3c71f5ff21d0a2c5062b
KrystianWieleba <kris_96@live.fr>
2018-05-08 22:47:43 +0200

    leaderboard test

--- app/src/main/java/com/example/kris/minigamesclash/ScoreTable.java
+++ app/src/main/java/com/example/kris/minigamesclash/ScoreTable.java
@@ -18,7 +18,11 @@ import java.util.ArrayList;
 
 public class ScoreTable extends AppCompatActivity {
 
-    TextView scoreTable = null;
+    TextView fistPlace = null;
+    TextView secondPlace = null;
+    TextView thirdPlace = null;
+    TextView fourthPlace = null;
+    TextView fifthPlace = null;
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference("leaderboard");
@@ -28,26 +32,51 @@ public class ScoreTable extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_table);
 
-        scoreTable =(TextView) findViewById(R.id.scoree);
+        fistPlace =(TextView) findViewById(R.id.score1);
+        secondPlace =(TextView) findViewById(R.id.score2);
+        thirdPlace =(TextView) findViewById(R.id.score3);
+        fourthPlace =(TextView) findViewById(R.id.score4);
+        fifthPlace =(TextView) findViewById(R.id.score5);
 
         int score = getIntent().getIntExtra("SCORE", 0);
 
-
-        myRef.child("id" + score ).setValue(score + "");
-
-
-
+        myRef.child("id" + score ).setValue(score+"");
 
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 //String value = dataSnapshot.child("id0").getValue(String.class);
-                //scoreTable.setText(value + "");
+                //scoreTable.setText(value);
                 //myRef.child("score").setValue(value);
 
-                for (DataSnapshot childs : dataSnapshot.getChildren()) {
-                    String list = childs.getValue().toString();
-                    scoreTable.setText(list);
+
+                int[] temp = new int[5];
+
+                //J'ai essayer de faire un genre de tri, mais puisque les childs garde leurs valeurs ça ne marche pas..
+                for (int i =0; i<=2; i++) {
+
+
+                    for (DataSnapshot childs : dataSnapshot.getChildren()) {
+                        String list = childs.getValue().toString();
+                        int res = Integer.parseInt(list);
+
+                        if (res > temp[i]) {
+                            temp[i] = res;
+
+
+                        }
+
+                    }
+
+
+
+                    fistPlace.setText("1 : " + temp[0]);
+                    secondPlace.setText("2 : " + temp[1]);
+                    thirdPlace.setText("3 : " + temp[2]);
+                    //fourthPlace.setText("4 : " + temp[3]);
+                    //fifthPlace.setText("5 : " + temp[4]);
+
+                    //myRef.child("id"+temp[0]).setValue(00);
 
                 }
             }
@@ -57,11 +86,7 @@ public class ScoreTable extends AppCompatActivity {
                 Log.w("Failed to read value.", error.toException());
             }
         });
-
-
-
     }
-
 }
 
 
