847303cdb343d2854eb66bb6e45c984840fc31f6
KrystianWieleba <kris_96@live.fr>
2018-05-09 17:16:04 +0200

    Test Firebase

--- app/src/main/java/com/example/kris/minigamesclash/ScoreTable.java
+++ app/src/main/java/com/example/kris/minigamesclash/ScoreTable.java
@@ -18,21 +18,21 @@ import java.util.ArrayList;
 
 public class ScoreTable extends AppCompatActivity {
 
-    TextView fistPlace = null;
+    TextView firstPlace = null;
     TextView secondPlace = null;
     TextView thirdPlace = null;
     TextView fourthPlace = null;
     TextView fifthPlace = null;
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference("leaderboard");
+    DatabaseReference myRef = database.getReference("Bubble Destroyer");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_table);
 
-        fistPlace =(TextView) findViewById(R.id.score1);
+        firstPlace =(TextView) findViewById(R.id.score1);
         secondPlace =(TextView) findViewById(R.id.score2);
         thirdPlace =(TextView) findViewById(R.id.score3);
         fourthPlace =(TextView) findViewById(R.id.score4);
@@ -40,47 +40,37 @@ public class ScoreTable extends AppCompatActivity {
 
         int score = getIntent().getIntExtra("SCORE", 0);
 
-        myRef.child("id" + score ).setValue(score+"");
 
-        myRef.addValueEventListener(new ValueEventListener() {
+        myRef.child("Leaderboard").push().child("score").setValue(score);
+
+        Query query = myRef.child("Leaderboard").orderByValue().limitToLast(5);
+
+        //myRef.limitToLast(5).toJSON();
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 //String value = dataSnapshot.child("id0").getValue(String.class);
                 //scoreTable.setText(value);
                 //myRef.child("score").setValue(value);
 
-
-                int[] temp = new int[5];
-
-                //J'ai essayer de faire un genre de tri, mais puisque les childs garde leurs valeurs ça ne marche pas..
-                for (int i =0; i<=2; i++) {
-
-
-                    for (DataSnapshot childs : dataSnapshot.getChildren()) {
-                        String list = childs.getValue().toString();
-                        int res = Integer.parseInt(list);
-
-                        if (res > temp[i]) {
-                            temp[i] = res;
-
-
-                        }
-
+                //just pour tester si le for parcourt tout
+                int i =0;
+                    for (DataSnapshot child : dataSnapshot.getChildren()) {
+                        String list = child.getValue().toString();
+                        firstPlace.setText(list);
+                        i++;
                     }
+                //firstPlace.setText(""+i);
 
-
-
-                    fistPlace.setText("1 : " + temp[0]);
-                    secondPlace.setText("2 : " + temp[1]);
-                    thirdPlace.setText("3 : " + temp[2]);
-
+                    //fistPlace.setText("1 : " + temp[0]);
+                    //secondPlace.setText("2 : " + temp[1]);
+                    //thirdPlace.setText("3 : " + temp[2]);
                     //fourthPlace.setText("4 : " + temp[3]);
                     //fifthPlace.setText("5 : " + temp[4]);
 
                     //myRef.child("id"+temp[0]).setValue(00);
-
                 }
-            }
 
             @Override
             public void onCancelled(DatabaseError error) {
