bcb84771beccd4a52fe53e19c25a42023a7edfee
CeluiQuiSait <eric.lavergne@telecom-sudparis.eu>
2018-05-16 11:30:00 +0200

    devlpmt de firebase Snail VS activity

--- app/src/main/java/com/example/kris/minigamesclash/SnailVSActivity.java
+++ app/src/main/java/com/example/kris/minigamesclash/SnailVSActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -13,23 +14,29 @@ import android.widget.Button;
 import android.os.CountDownTimer;
 import android.util.DisplayMetrics;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SnailVSActivity extends AppCompatActivity implements OnClickListener {
 
     private ImageView snail1;
-    private ImageView snail2;
+    private ImageView snailAdv;
     private TextView temps;
     private Button retour;
     private View layout;
     private float posSnail1=50;
-    private float posSnail2=50;
+    private float posSnailAdv=50;
     private CountDownTimer countDownTimer;
     private boolean debutTimer=false;
     private DisplayMetrics metrics;
     private float longueurEcran;
     private float densiteEcran;
+    private String refSnail1;
+    private String refSnailAdv;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference("Snail");
 
@@ -40,20 +47,62 @@ public class SnailVSActivity extends AppCompatActivity implements OnClickListene
 
         temps=(TextView) findViewById(R.id.temps);
         snail1=(ImageView) findViewById(R.id.snail1);
-        snail2=(ImageView) findViewById(R.id.snail2);
+        snailAdv=(ImageView) findViewById(R.id.snail2);
         layout=(View) findViewById(R.id.layout);
         //le bouton de retour au menu est d'abord caché
         retour=(Button) findViewById(R.id.retour);
         retour.setVisibility(View.INVISIBLE);
         layout.setOnClickListener(this);
         retour.setOnClickListener(this);
-        snail1.setX(50);
-        snail2.setX(50);
-
         metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
         longueurEcran=metrics.widthPixels;
         densiteEcran=metrics.density;
+        snail1.setX(50);
+        snailAdv.setX(50);
+
+        //Phase d'initialisation des identifiants (qu'on pourra peut-être mettre au debut du vs)
+        int temp = (int)Math.random()*1000000;
+        refSnail1=Integer.toString(temp);
+        myRef.child(refSnail1);
+
+        myRef.child(refSnail1).setValue(snail1);
+
+        ValueEventListener eventL1 = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot childs : dataSnapshot.getChildren()) {
+                    String refTemp=childs.getKey();
+                    if (refTemp!=refSnail1){
+                        refSnailAdv=refTemp;
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("Failed to read value.", databaseError.toException());
+            }
+        };
+
+        while (refSnailAdv==null) {
+            myRef.addListenerForSingleValueEvent(eventL1);
+        }
+
+        myRef.removeEventListener(eventL1);
+
+
+        myRef.child(refSnailAdv).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                posSnailAdv=Integer.parseInt(dataSnapshot.getValue().toString());
+                snailAdv.setX(posSnailAdv);
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("Failed to read value.", databaseError.toException());
+            }
+        });
     }
 
     //il n'y a pas des choses variées à cliquer, l'activité implémente directement OnClickListener
@@ -85,6 +134,7 @@ public class SnailVSActivity extends AppCompatActivity implements OnClickListene
         else {
             posSnail1 += 12;
             snail1.setX(posSnail1);
+            myRef.child(refSnail1).setValue(posSnail1);
         }
 
         if (v.getId()==retour.getId()){
@@ -93,3 +143,5 @@ public class SnailVSActivity extends AppCompatActivity implements OnClickListene
         }
     }
 }
+
+//Il faudra ajouter une ligne à la fin pour supprimer le child avc l'id aleatoire (pour l'instant le faire à la main)
