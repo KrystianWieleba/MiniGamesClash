be4e7697c1cee45a25ce8099fde0068184d08c08
Christos <christos.katsoulakis@telecom-sudparis.eu>
2018-05-21 22:20:34 +0200

    Credits

--- app/src/main/java/com/example/kris/minigamesclash/credits.java
+++ app/src/main/java/com/example/kris/minigamesclash/credits.java
@@ -1,13 +1,31 @@
 package com.example.kris.minigamesclash;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class credits extends AppCompatActivity {
+
+    Button boutonRetour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
+
+        boutonRetour = (Button) findViewById(R.id.boutonRetour);
+
+        boutonRetour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+
+            }
+        });
+
     }
 }
