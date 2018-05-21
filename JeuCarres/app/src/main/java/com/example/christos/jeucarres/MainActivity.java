f27bee0ff35d77b253fa57a8b4d49e2a6bbccd56
Christos <christos.katsoulakis@telecom-sudparis.eu>
2018-05-21 10:51:28 +0200

    JeuCarres vf

--- JeuCarres/app/src/main/java/com/example/christos/jeucarres/MainActivity.java
+++ JeuCarres/app/src/main/java/com/example/christos/jeucarres/MainActivity.java
@@ -14,9 +14,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     int res[][] = new int[7][7];
     ArrayList<Button> buttons = new ArrayList<>();
-    int player = 1;
-    int bleu = 0;
-    int rouge = 0;
+    int player = 1; // quel joueur joue ?
+    int bleu = 0;   //score du joueur 1
+    int rouge = 0;  // score du joueur 2
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +48,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button but63 = (Button) findViewById(R.id.button63);
         Button but65 = (Button) findViewById(R.id.button65);
 
+        /*
         Button butA = (Button) findViewById(R.id.buttonA);
         Button butB = (Button) findViewById(R.id.buttonB);
         Button butC = (Button) findViewById(R.id.buttonC);
@@ -57,16 +58,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button butG = (Button) findViewById(R.id.buttonG);
         Button butH = (Button) findViewById(R.id.buttonH);
         Button butI = (Button) findViewById(R.id.buttonI);
-
-        res[1][1] = 0;
-        res[1][3] = 0;
-        res[1][5] = 0;
-        res[3][1] = 0;
-        res[3][3] = 0;
-        res[3][5] = 0;
-        res[5][1] = 0;
-        res[5][3] = 0;
-        res[5][5] = 0;
+        */
 
         buttons.add(but01);
         buttons.add(but03);
@@ -94,7 +86,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttons.add(but65);
 
         for (Button but : buttons) {
-            but.setBackgroundDrawable(null);
+            but.setBackgroundDrawable(null);  // Background des boutons à null
             but.setOnClickListener(this);
         }
 
@@ -104,9 +96,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
+        // Vérification que la case n'a pas été jouée
+
         if (v.getBackground() != null) {
             return;
         }
+
+        // Prise en compte dans le tableau des cases jouées
 
         switch(v.getId()) {
             case R.id.button01:
@@ -183,6 +179,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
 
+        // Matérialisation de la case jouée par un changement de couleur
+
         Drawable playercolor;
         if (player ==1) {
             playercolor = ContextCompat.getDrawable(this,R.drawable.carrebleu);
@@ -192,9 +190,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         v.setBackgroundDrawable(playercolor);
 
+        // Conditions pour obtenir un point (1point= 1 carré complété)
+
         if (res[0][1] != 0 && res[1][0] != 0 && res[1][2] != 0 && res[2][1] != 0 && res[1][1] == 0) {
             res[1][1] = player;
-            R.id.buttonA.setBackground(playercolor);
             if (player == 1) {
                 bleu++;
             } else {
@@ -274,6 +273,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
 
+        // Conditions de fin du jeu (tous les carrés ont été entourés) et de victoire
+
         if (bleu + rouge == 9) {
             if (bleu > rouge) {
                 player1Win();
@@ -282,6 +283,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
 
+        // Si le jeu n'est pas fini, changement de joueur
+
         if (player == 1) {
             player = 2;
         } else {
@@ -289,6 +292,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
     }
+
+    // Transition vers les autres activités
+    // A changer pour uniformisation
 
     void player1Win(){
         Toast.makeText(this, "Player 1 wins !", Toast.LENGTH_SHORT).show();
