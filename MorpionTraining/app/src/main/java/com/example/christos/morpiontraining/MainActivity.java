839e5c59418bfa8633774cbfd0764a0ad2fd8245
Christos <christos.katsoulakis@telecom-sudparis.eu>
2018-05-22 19:30:38 +0200

    MorpionTraining

--- MorpionTraining/app/src/main/java/com/example/christos/morpiontraining/MainActivity.java
+++ MorpionTraining/app/src/main/java/com/example/christos/morpiontraining/MainActivity.java
@@ -13,8 +13,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     String res[][] = new String[3][3];
     ArrayList<Button> buttons = new ArrayList<>();
-    boolean player1 = true;
     int turn = 0;
+
+    Button but00;
+    Button but01;
+    Button but02;
+    Button but10;
+    Button but11;
+    Button but12;
+    Button but20;
+    Button but21;
+    Button but22;
+
 
 
     @Override
@@ -28,15 +38,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
 
-        Button but00 = (Button) findViewById(R.id.button00);
-        Button but01 = (Button) findViewById(R.id.button01);
-        Button but02 = (Button) findViewById(R.id.button02);
-        Button but10 = (Button) findViewById(R.id.button10);
-        Button but11 = (Button) findViewById(R.id.button11);
-        Button but12 = (Button) findViewById(R.id.button12);
-        Button but20 = (Button) findViewById(R.id.button20);
-        Button but21 = (Button) findViewById(R.id.button21);
-        Button but22 = (Button) findViewById(R.id.button22);
+        but00 = (Button) findViewById(R.id.button00);
+        but01 = (Button) findViewById(R.id.button01);
+        but02 = (Button) findViewById(R.id.button02);
+        but10 = (Button) findViewById(R.id.button10);
+        but11 = (Button) findViewById(R.id.button11);
+        but12 = (Button) findViewById(R.id.button12);
+        but20 = (Button) findViewById(R.id.button20);
+        but21 = (Button) findViewById(R.id.button21);
+        but22 = (Button) findViewById(R.id.button22);
 
         buttons.add(but00);
         buttons.add(but01);
@@ -62,104 +72,109 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             return;
         }
 
-        if (player1) {
-            if (v.getId() == R.id.button00) {
-                res[0][0] = "x";
-                v.setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.morpioncross));
-            } else if (v.getId() == R.id.button01) {
-                res[0][1] = "x";
-                v.setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.morpioncross));
-            } else if (v.getId() == R.id.button02) {
-                res[0][2] = "x";
-                v.setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.morpioncross));
-            } else if (v.getId() == R.id.button10) {
-                res[1][0] = "x";
-                v.setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.morpioncross));
-            } else if (v.getId() == R.id.button11) {
-                res[1][1] = "x";
-                v.setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.morpioncross));
-            } else if (v.getId() == R.id.button12) {
-                res[1][2] = "x";
-                v.setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.morpioncross));
-            } else if (v.getId() == R.id.button20) {
-                res[2][0] = "x";
-                v.setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.morpioncross));
-            } else if (v.getId() == R.id.button21) {
-                res[2][1] = "x";
-                v.setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.morpioncross));
-            } else if (v.getId() == R.id.button22) {
-                res[2][2] = "x";
-                v.setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.morpioncross));
-            }
-        } else {
 
-            // On construit un tableau qui prend l'état du jeu
-
-            String a = res[0][0];
-            String b = res[0][1];
-            String c = res[0][2];
-            String d = res[1][0];
-            String e = res[1][1];
-            String f = res[1][2];
-            String g = res[2][0];
-            String h = res[2][1];
-            String i = res[2][2];
-
-            String[] tab = {a, b, c, d, e, f, g, h, i};
-
-            // On génère un nombre aléatoire entre 0 et 8, et on vérifie que la case correspondante est vide
-
-            int n = (int) Math.round(Math.random() * 10);
-
-            while (n == 9 || tab[n] != " ") {
-                n = (int) Math.round(Math.random() * 10);
-            }
-
-            if (n == 0) {
-                res[0][0] = "o";
-                v.setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.morpionround));
-            } else if (n == 1) {
-                res[0][1] = "o";
-                v.setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.morpionround));
-            } else if (n == 2) {
-                res[0][2] = "o";
-                v.setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.morpionround));
-            } else if (n ==  3) {
-                res[1][0] = "o";
-                v.setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.morpionround));
-            } else if (n == 4) {
-                res[1][1] = "o";
-                v.setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.morpionround));
-            } else if (n == 5) {
-                res[1][2] = "o";
-                v.setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.morpionround));
-            } else if (n == 6) {
-                res[2][0] = "o";
-                v.setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.morpionround));
-            } else if (n == 7) {
-                res[2][1] = "o";
-                v.setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.morpionround));
-            } else if (n == 8) {
-                res[2][2] = "o";
-                v.setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.morpionround));
-            }
+        if (v.getId() == R.id.button00) {
+            res[0][0] = "x";
+            v.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.morpioncross));
+        } else if (v.getId() == R.id.button01) {
+            res[0][1] = "x";
+            v.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.morpioncross));
+        } else if (v.getId() == R.id.button02) {
+            res[0][2] = "x";
+            v.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.morpioncross));
+        } else if (v.getId() == R.id.button10) {
+            res[1][0] = "x";
+            v.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.morpioncross));
+        } else if (v.getId() == R.id.button11) {
+            res[1][1] = "x";
+            v.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.morpioncross));
+        } else if (v.getId() == R.id.button12) {
+            res[1][2] = "x";
+            v.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.morpioncross));
+        } else if (v.getId() == R.id.button20) {
+            res[2][0] = "x";
+            v.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.morpioncross));
+        } else if (v.getId() == R.id.button21) {
+            res[2][1] = "x";
+            v.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.morpioncross));
+        } else if (v.getId() == R.id.button22) {
+            res[2][2] = "x";
+            v.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.morpioncross));
         }
 
         turn++;
 
         if (Win()) {
-            if (player1) {
-                player1Win();
-            } else {
-                player2Win();
-            }
+            player1Win();
         } else if (turn == 9) {
             draw();
-        } else {
-            player1 = !player1;
         }
 
+
+        // On construit un tableau qui prend l'état du jeu
+
+        String a = res[0][0];
+        String b = res[0][1];
+        String c = res[0][2];
+        String d = res[1][0];
+        String e = res[1][1];
+        String f = res[1][2];
+        String g = res[2][0];
+        String h = res[2][1];
+        String i = res[2][2];
+
+        String[] tab = {a, b, c, d, e, f, g, h, i};
+
+        // On génère un nombre aléatoire entre 0 et 8, et on vérifie que la case correspondante est vide
+
+        int n = (int) Math.round(Math.random() * 10);
+
+        while (n == 9 || tab[n] != " ") {
+            n = (int) Math.round(Math.random() * 10);
+        }
+
+        if (n == 0) {
+            res[0][0] = "o";
+            but00.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.morpionround));
+        } else if (n == 1) {
+            res[0][1] = "o";
+            but01.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.morpionround));
+        } else if (n == 2) {
+            res[0][2] = "o";
+            but02.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.morpionround));
+        } else if (n == 3) {
+            res[1][0] = "o";
+            but10.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.morpionround));
+        } else if (n == 4) {
+            res[1][1] = "o";
+            but11.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.morpionround));
+        } else if (n == 5) {
+            res[1][2] = "o";
+            but12.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.morpionround));
+        } else if (n == 6) {
+            res[2][0] = "o";
+            but20.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.morpionround));
+        } else if (n == 7) {
+            res[2][1] = "o";
+            but21.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.morpionround));
+        } else if (n == 8) {
+            res[2][2] = "o";
+            but22.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.morpionround));
+        }
+
+
+        turn++;
+
+        if (Win()) {
+            player2Win();
+        } /*else if (turn == 9) {
+            draw();
+        }
+        */
+
     }
+
+
 
     boolean Win() {
 
